2d6196c0c57d714409db923cd9a36dc492234176
ismarulop <ismarulopdeveloper@gmail.com>
2022-11-16 11:26:56 +0100

    first commit

---
+++ src/test/java/com/bermairlines/analytics/AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bermairlines.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
